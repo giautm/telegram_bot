4fed8d9b4fa2ff9c072698caa3fe66e6f20d22d9
Tobias <tobiasmichael@mail.de>
2018-02-23 17:34:20 +0100

    wake fixed

--- bot/bot.py
+++ bot/bot.py
@@ -133,9 +133,7 @@ def wake(bot, update):
     if os.path.isfile('wake.txt'):
         with open('wake.txt') as wake_file:
             address = filter(None, (line.rstrip() for line in wake_file))[0]
-            command = address + ' &> /dev/null'
-            print command
-            call(['wakeonlan', command])
+            call(['wakeonlan', address])
             update.message.reply_text('Wake executed.')
 
 
